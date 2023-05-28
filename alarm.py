2e7d080a139b25a91fb7a0283f83fe894faad9d3
deepshika29 <69142774+deepshika29@users.noreply.github.com>
2023-05-28 17:29:44 +0530

    Add files via upload

---
+++ alarm.py
@@ -0,0 +1,17 @@
+import time
+
+# Calculate the duration of 5 minutes in seconds
+duration = 5 * 60
+
+# Get the current time
+start_time = time.time()
+
+# Loop until the specified duration has elapsed
+while time.time() - start_time < duration:
+    # Perform any necessary operations here
+    # Replace this with your actual code
+    
+    # Sleep for a short interval (e.g., 1 second) to avoid excessive CPU usage
+    time.sleep(1)
+
+print("Script execution completed.")
